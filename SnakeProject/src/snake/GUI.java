5879def34b3e6dcbdf51e8d1f265fb43ee7e3d6f
powerslaver85 <ram.schenk@gmail.com>
2015-01-15 14:51:21 +0100

    SnakeProject Added

---
+++ SnakeProject/src/snake/GUI.java
@@ -0,0 +1,104 @@
+package snake;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ * Die Klasse GUI stellt das Fenster (JFrame) und  das Spielfeld (JPanel) des Snake-Spiels zur Verf�gung. Implentiert den KeyListener
+ * @author   A. Scheidegger, M.Frieden
+ * NOTE: FREMDCODE.
+ */
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame implements KeyListener {
+
+  /** Referenz auf das Game-Objekt. */
+  private Game game;
+
+  /** Das JPanel, welches als Spielfeld dient. Anonyme innere Klasse. */
+  private JPanel field = new JPanel() {
+    /**
+     * Wird automatisch aufgerufen, wenn das Fenster neu gezeichnet werden
+     * muss. Delegiert das Zeichnen des Spiels an das Game-Objekt.
+     * @param g Graphics-Objekt zum Zeichnen
+     * NOTE: FREMDCODE.
+     */
+	  
+	  
+    @Override
+    public void paintComponent(Graphics g){
+      game.draw(g);
+    }
+  };
+
+  /**
+   * GUI Constructor 
+   * Initialisiert den Frame. Registriert das Game-Objekt
+   * als KeyListener.
+   * NOTE: FREMDCODE.
+   */
+  public GUI(Game game) {
+    this.game = game;
+    setSize(600, 400);
+    setTitle("Snake V1.0 Final");
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    setContentPane(field);
+    addKeyListener(this);
+    setVisible(true);
+  }
+
+  /**
+   * Gibt die Breite des Spielfeldes zurueck.
+   * @return Breite des Spielfeldes.
+   */
+  public int getWidth() {
+    return field.getWidth();
+  }
+
+  /**
+   * Gibt die Hoehe des Spielfeldes zurueck.
+   * @return Hoehe des Spielfeldes
+   */
+  public int getHeight() {
+    return field.getHeight();
+  }
+  
+	 /***
+	 * Die Key-Listener-Methoden
+	 */
+	
+	/**
+	 * Methode die die Richtung des snakenkopfs bestimmt,
+	 * anhand der gedrueckten Tasten. Die Richtung laesst sich mit den
+	 * Pfeiltasten oder mit WASD bestimmen
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keycode = e.getKeyCode();
+		if (keycode == KeyEvent.VK_UP || keycode == KeyEvent.VK_W)
+			game.snake.direction = "oben";
+		if (keycode==KeyEvent.VK_DOWN || keycode == KeyEvent.VK_S)
+			game.snake.direction = "unten";
+		if(keycode==KeyEvent.VK_RIGHT || keycode == KeyEvent.VK_D)
+			game.snake.direction= "rechts";
+		if(keycode== KeyEvent.VK_LEFT || keycode == KeyEvent.VK_A)
+			game.snake.direction = "links";
+			
+		
+	}
+	/**
+	 * Leere Methoden des KeyListeners, sie haben keine Funktion.
+	 */
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+}
---
+++ SnakeProject/src/snake/Game.java
@@ -0,0 +1,156 @@
+package snake;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Vector;
+
+/**
+ * Game enthaelt die main() Methode und dient als Runner fuer das Snake Spiel,
+ * die Klasse enthaelt ausserdem die Spielsteuerung und liefert die optischen
+ * Spielelemente.
+ * @Version	1.0 Final
+ * @Author	RSCHENK
+ */
+public class Game {
+	
+	/**
+	 * GUI -> JPanel Element fuer die Fensterdarstellung.
+	 */
+	private GUI gui;
+	/**
+	 * Player Objekt (snake)
+	 */
+	public Snake snake;
+	/**
+	 * Border des Spielbereichs
+	 */
+	public Border border;
+	/**
+	 * Vektor der als Behaelter fuer alle Spielelemente dient
+	 */
+	public Vector<GameElement> gameElements;
+	
+	/**
+	 * Schrittlaenge
+	 */
+	private int STEP = 10;
+
+	/**
+	 * Game Constructor, erzeugt alle Elemente auf dem Spielfeld
+	 * inkl. dem Spielfeld selbst.
+	 */
+	public Game()
+	{
+		this.gameElements = new Vector<GameElement>();
+		this.snake = new Snake(400,80);
+		this.border =  new Border(580,345);
+	}
+	/**
+	 * RUNNER
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.gameElements.add(game.snake);
+		game.gameElements.add(game.border);
+		game.setDiamonds(8);
+			
+		game.gui = new GUI(game);
+		game.checkState();
+		
+		
+	}
+	
+	
+	/**
+	 * Die draw Methode wird in mehreren anderen Klassen als Ableitung
+	 * genutzt um Spielelemente zu zeichnen, sie spielt eine zentrale Rolle
+	 * bei dem Snake Programm.
+	 * @param Graphics Grafik
+	 */
+	public void draw(Graphics g)
+	{
+		for(GameElement se : gameElements)
+		{
+			se.draw(g);
+		}
+	}
+	
+	
+	
+	/**
+	 * checkState prueft ob die Schlange am leben ist bzw. ob etwas
+	 * (Kollision mit Border/Sich Selbst) diesen Status geaendert hat.
+	 */
+	public void checkState()
+	{
+		while(snake.isAlive && border.isAlive)
+		{
+			snake.move(STEP);
+			checkGameElement(snake);
+			checkGameOver();
+			gui.repaint();
+			try
+			{
+				Thread.sleep(200);
+			}catch(InterruptedException e){}
+			
+		}
+	}
+	
+	
+	/**
+	 * checkGameElement prueft spezifisch ob die Schlange mit einem 
+	 * gameElement kollidiert ist.
+	 * @param s Snake
+	 */
+	public void checkGameElement(Snake s)
+	{
+		for (int i = 0; i< gameElements.size(); i++)
+		{
+			GameElement element = gameElements.elementAt(i);
+			element.collisionCheck(s);
+			//Pruefe ob sichtbar.
+			if(!element.isVisible)
+				gameElements.remove(element);
+			
+		}
+	}
+	
+	/**
+	 * checkGameOver prueft ob die Bedingung erfuellt wurde die fuer ein
+	 * Game Over erforderlich sind.
+	 */
+	private void checkGameOver()
+	{
+		if(!snake.isAlive)
+			this.gui.setTitle("Game Over! Eigenkollision");
+		if(!border.isAlive)
+			this.gui.setTitle("Game Over! Borderkollision");
+		if(gameElements.size() == 2) //True wenn alle Diamanten gefressen wurden.
+		{
+			snake.isAlive = false;
+			gui.setTitle("Spiel gewonnen.");
+		}
+		
+	}
+	
+	
+	/**
+	 * Mithilfe des Randomizers werden die Diamanten auf dem Spielfeld
+	 * platziert. x, y Position und der Wert werden jeweils vom Randomizer
+	 * zufaellig generiert, die Anzahl wird beim Aufruf vorgegeben.
+	 * @param anzahl
+	 */
+	private void setDiamonds(int anzahl)
+	{
+		for(int i = 0; i<anzahl;i++)
+		{
+			int px = Randomizer.randomValue(20, 440);
+			int py = Randomizer.randomValue(20, 340);
+			int val = Randomizer.randomValue(1, 8);
+			this.gameElements.add(new Diamond(new Point(px,py),val));
+		}
+	}
+
+}
